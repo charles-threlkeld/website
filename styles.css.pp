417e124d38acacd3767decf31b3c851908ad9d6e
Chas <chathrel@indiana.edu>
2018-09-03 12:42:59 -0400

    Added fonts and css font specs

--- styles.css.pp
+++ styles.css.pp
@@ -6,6 +6,38 @@
 ◊(define multiplier 1.3)
 ◊(define tiny 0.1)
 
+@font-face {
+    font-family: "Charter";
+    src: url('fonts/charter_regular-webfont.woff') format('woff'),
+    url('fonts/charter_regular-webfont.ttf') format('truetype');
+    font-weight: normal;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: "Charter";
+    src: url('fonts/charter_bold-webfont.woff') format('woff'),
+    url('fonts/charter_bold-webfont.ttf') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+}
+
+@font-face {
+    font-family: "Charter";
+    src: url('fonts/charter_italic-webfont.woff') format('woff'),
+    url('fonts/charter_italic-webfont.ttf') format('truetype');
+    font-weight: normal;
+    font-style: italic;
+}
+
+@font-face {
+    font-family: "Charter";
+    src: url('fonts/charter_bold_italic-webfont.woff') format('woff'),
+    url('fonts/charter_bold_italic-webfont.ttf') format('truetype');
+    font-weight: bold;
+    font-style: italic;
+}
+
 body {
     margin: ◊|edge|em;
     border: ◊|inner|em double ◊|color|;
